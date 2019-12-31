9eb4a353accdf8fb89b861281a243ebceb243137
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-12-31 17:27:04 +0100

    .

--- runme.sh
+++ runme.sh
@@ -1,15 +1,16 @@
 export BIBINPUTS=":.:/home/vruiz/bibs/:"
 make clean
 make index.pdf
-grep citation ~/probabilistic_models/index.aux >> index.aux
-grep citation ~/Shannon-Fano_coding/index.aux >> index.aux
-grep citation ~/Huffman_coding/index.aux >> index.aux
-grep citation ~/arithmetic_coding/index.aux >> index.aux
-grep citation ~/move-to-front_transform/index.aux >> index.aux
-grep citation ~/context-based_text_predictive_transform/index.aux >> index.aux
-grep citation ~/unary_coding/index.aux >> index.aux
-grep citation ~/Golomb-Rice_coding/index.aux >> index.aux
-grep citation ~/gzip/index.aux >> index.aux
+grep citation ~/probabilistic_models/index.aux >> citations
+grep citation ~/Shannon-Fano_coding/index.aux >> citations
+grep citation ~/Huffman_coding/index.aux >> citations
+grep citation ~/arithmetic_coding/index.aux >> citations
+grep citation ~/move-to-front_transform/index.aux >> citations
+grep citation ~/context-based_text_predictive_transform/index.aux >> citations
+grep citation ~/unary_coding/index.aux >> citations
+grep citation ~/Golomb-Rice_coding/index.aux >> citations
+grep citation ~/gzip/index.aux >> citations
+cat citations >> index.aux
 bibtex index
 make index.html
 
